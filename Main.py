ca1ecf1b1fac2a29220c0add49dafd30c9422f94
franman732 <nathanielmitri732@email.com>
2026-05-13 15:58:06 -0700

    Started with knight.

--- Main.py
+++ Main.py
@@ -3,14 +3,14 @@ import numpy as np
 pieces = {0:"no", 1:"pawn", 2:"pawn", 3:"pawn", 4:"pawn", 5:"pawn", 6:"pawn", 7:"pawn", 8:"pawn", 9:"rook", 10:"knight", 11:"bishop", 12:"queen", 13:"king", 14:"bishop", 15:"knight", 16:"rook", #lowercase is black, capital is white
           17:"PAWN  ", 18:"PAWN", 19:"PAWN", 20:"PAWN", 21:"PAWN", 22:"PAWN", 23:"PAWN", 24:"PAWN", 25:"ROOK", 26:"KNIGHT", 27:"BISHOP", 28:"QUEEN", 29:"KING", 30:"BISHOP", 31:"KNGIHT", 32:"ROOK"} #Black is 0, white is 1
 
-board = np.array(9, 10, 11, 12, 13, 14, 15, 16, # top is black/lowercase/0 ; bottom is white/uppercase/1
+board = np.array([9, 10, 11, 12, 13, 14, 15, 16, # top is black/lowercase/0 ; bottom is white/uppercase/1
                  1, 2, 3, 4, 5, 6, 7, 8,
                  0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0,
                  17, 18, 19, 20, 21, 22, 23, 24,
-                 25, 26, 27, 28, 29, 30, 31, 32)
+                 25, 26, 27, 28, 29, 30, 31, 32])
 
 def get_color(board, index):
     if board[index] == 0:
@@ -101,7 +101,23 @@ def determine_rook_moves(board, moves, start):
         
 
 def determine_knight_moves(board, moves, start):
-    return
+    color = get_color(board, start)
+    if get_color(board[start - 6]) != color:
+        moves.append(start, start - 6)
+    if get_color(board(start - 10)) != color:
+        moves.append(start, start - 10)
+    if get_color(board(start - 15)) != color:
+        moves.append(start, start - 15)
+    if get_color(board(start - 17)) != color:
+        moves.append(start, start - 17)
+    if get_color(board(start + 6)) != color:
+        moves.append(start, start + 6)
+    if get_color(board(start + 10)) != color:
+        moves.append(start, start + 10)
+    if get_color(board(start + 15)) != color:
+        moves.append(start, start + 15)
+    if get_color(board(start + 17)) != color:
+        moves.append(start, start + 17)
 
 def determine_bishop_moves(board, moves, start):
     return
